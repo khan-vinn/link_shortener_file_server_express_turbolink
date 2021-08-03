6a995c38e70d60f53814d99a19a651fb3e0c2d99
Shakhan <khan.vinn@gmail.com>
2021-08-03 21:42:20 +0500

    middleware flash messages on errors)

--- routes/middleware.js
+++ routes/middleware.js
@@ -2,10 +2,12 @@ function ensureNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next()
     }
+    req.flash("error", "Please login or sign up to coninue")
     res.redirect("/auth")
 }
 function ensureFromAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
+        req.flash("error", "You will login to app/ please logout to continue")
         return res.redirect("/")
     }
     next()
