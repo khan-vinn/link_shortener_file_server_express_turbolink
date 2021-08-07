7000dfd1660767de3abcd947b3b03318496f189d
Shakhan <khan.vinn@gmail.com>
2021-08-07 13:59:15 +0500

    links routes and views basics'

--- routes/link.js
+++ routes/link.js
@@ -1,17 +1,44 @@
 const express = require('express');
+const dns = require("dns");
 const { ensureNotAuthenticated, flashMessageProvideToRender } = require('./middleware');
 const router = express.Router();
 
 router.get("/", flashMessageProvideToRender,
     ensureNotAuthenticated,
     (req, res, next) => {
-        res.send("Will render link create form")
+        res.render("links/form")
     })
 
 router.post("/", ensureNotAuthenticated,
     (req, res, next) => {
         if (typeof (req.body.link) === "string" && (req.body.link.length > 5)) {
-            next()
+            var protocolHost;
+            var urlDns;
+            try {
+                protocolHost = req.body.link.split("//")[1];
+            } catch (error) {
+                req.flash("error", `${error.name} :: ${error.message}`)
+                req.flash("error", "Link should be valid like https://example.com/link/wich/you/want/shortened")
+                return res.redirect("/l")
+            }
+            try {
+                urlDns = protocolHost.split("/")[0];
+            } catch (error) {
+                req.flash("error", `${error.name} :: ${error.message}`)
+                req.flash("error", "DNS, like example.com should be representive")
+                return res.redirect("/l")
+            }
+            dns.lookup(urlDns, (error) => {
+                if (error) {
+                    req.flash("error", `${error.name} :: ${error.message}`)
+                    req.flash("error", "DNS should be correctly work")
+                    return res.redirect("/l")
+                }
+                return next()
+            })
+        } else {
+            req.flash("error", "Link length should be more than 8")
+            res.redirect("/l")
         }
     },
     (req, res, next) => {
