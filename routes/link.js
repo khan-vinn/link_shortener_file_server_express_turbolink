65c4570713211687eeea4d9d5336664836d8f1d4
Shakhan <khan.vinn@gmail.com>
2021-08-07 10:42:00 +0500

    prettier

--- routes/link.js
+++ routes/link.js
@@ -2,25 +2,30 @@ const express = require('express');
 const { ensureNotAuthenticated, flashMessageProvideToRender } = require('./middleware');
 const router = express.Router();
 
-router.route("/")
-    .get(flashMessageProvideToRender,
-        ensureNotAuthenticated,
-        (req, res, next) => {
-            res.send("Will render link create form")
-        })
-    .post(ensureNotAuthenticated,
-        (req, res, next) => {
-            if (typeof (req.body.link) === "string" && (req.body.link.length > 5)) {
-                next()
-            }
-        }, (req, res, next) => { })
-    .get("/:id", (req, res, next) => {
-        res.send("will redirect to original link")
+router.get("/", flashMessageProvideToRender,
+    ensureNotAuthenticated,
+    (req, res, next) => {
+        res.send("Will render link create form")
     })
-    .get("/:id/stats",
-        ensureNotAuthenticated,
-        (req, res, next) => {
-            res.send("Will render statistic of link")
-        })
+
+router.post("/", ensureNotAuthenticated,
+    (req, res, next) => {
+        if (typeof (req.body.link) === "string" && (req.body.link.length > 5)) {
+            next()
+        }
+    },
+    (req, res, next) => {
+        res.send("will post link")
+    })
+
+router.get("/:id", (req, res, next) => {
+    res.send("will redirect to original link")
+})
+
+router.get("/:id/stats",
+    ensureNotAuthenticated,
+    (req, res, next) => {
+        res.send("Will render statistic of link")
+    })
 
 module.exports = router;
