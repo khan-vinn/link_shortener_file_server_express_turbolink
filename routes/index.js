c4a0bc454fcf892d3f78a199bde8c1891c4c9268
Shakhan <khan.vinn@gmail.com>
2021-08-09 16:21:29 +0500

    user information update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const { Link, File } = require('../models');
 const { ensureNotAuthenticated } = require('./middleware');
 const router = express.Router();
 
 router.get('/', ensureNotAuthenticated, function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  Link.find({ _lord: req.user._id }, (err, links) => {
+    if (err) {
+      console.log(err)
+    }
+    File.find({ _lord: req.user._id }, (err, files) => {
+      if (err) {
+        console.log(err)
+      }
+      res.render("users/personal-page", { user: req.user, links, files });
+    })
+  })
 });
 
 router.get('/@:id?', function (req, res, next) {
