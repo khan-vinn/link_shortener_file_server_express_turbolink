afcdaaf3c1ef2edcd1294d57058b32cd481ccbfc
Shakhan <khan.vinn@gmail.com>
2021-08-18 18:45:13 +0000

    auth routes to promise api

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const passport = require("passport");
 const bcrypt = require("bcrypt");
 const { User } = require('../models');
 const { ensureFromAuthenticated, flashMessageProvideToRender, ensureNotAuthenticated } = require('./middleware');
+const { authParamsValidate } = require('../middleware/auth');
 const router = express.Router();
 
 router.get("/", ensureFromAuthenticated, flashMessageProvideToRender, (req, res) => {
@@ -10,23 +11,7 @@ router.get("/", ensureFromAuthenticated, flashMessageProvideToRender, (req, res)
 })
 
 router.post("/signin",
-    (req, res, next) => {
-        if (typeof (req.body.username) === "string"
-            && (req.body.username.length > 4)
-            && (req.body.username.length < 18)) {
-            if (typeof (req.body.password) === "string"
-                && (req.body.password.length > 5)) {
-                next()
-            } else {
-                req.flash("error", "too short or long username min length is 5 and max length is 17")
-                res.redirect("/auth")
-            }
-        } else {
-            req.flash("error", "too short password/ min length of password is 6")
-            res.redirect("/auth")
-        }
-    },
-    ensureFromAuthenticated,
+    authParamsValidate, ensureFromAuthenticated,
     passport.authenticate('local', { failureRedirect: '/auth', failureFlash: "Password or login incorrected" }),
     (req, res) => {
         res.redirect("/")
@@ -39,110 +24,150 @@ router.get("/logout", ensureNotAuthenticated, (req, res, next) => {
 })
 
 router.post("/update", ensureNotAuthenticated, (req, res, next) => {
-    if (req.body.username && req.body.username.length > 4) {
-        User.findOneAndUpdate({ _id: req.user._id, username: req.user.username }, { username: req.body.username }, { new: true }, (err, user) => {
-            if (err) {
-                req.flash("error", `${err.name}::${err.message}`)
+    const { username, old_pass, new_pass, confirm_new_pass } = req.body
+
+    if (username && typeof (username) === "string" && username.length > 4) {
+        User.findByIdAndUpdate({ _id: req.user._id, username: req.user.username }, { username }, { new: true })
+            .then(updatedUser => {
+                if (!updatedUser) {
+                    req.flash("error", "Don't updated user info")
+                    return res.redirect("/")
+                } else {
+                    req.flash("success", "User info successufully updated")
+                    return res.redirect("/")
+                }
+            })
+            .catch(e => {
+                req.flash("error", `${e.name}::${e.message}`)
                 return res.redirect("/")
-            } else if (!user) {
-                req.flash("error", "Dont update username")
-                return res.redirect("/")
-            }
-            req.flash("success", "User name successfuly updated")
-            return res.redirect("/")
-        })
-    } else if (req.body.old_pass && req.body.new_pass && req.body.confirm_new_pass) {
-        if (req.body.old_pass.length > 5) {
-            if (req.body.new_pass === req.body.confirm_new_pass) {
-                bcrypt.compare(req.body.old_pass, req.user.password, (err, data) => {
-                    if (err) {
-                        req.flash("error", "error to compare passwords/ try later")
-                        return res.redirect("/")
-                    } else if (!data) {
-                        console.log(data)
-                        req.flash("error", "incorrect old password")
-                        return res.redirect("/")
-                    }
-                    hash = bcrypt.hash(req.body.new_pass, 12, (err, data) => {
-                        if (err) {
-                            req.flash("error", "Error to hashing new password")
-                            return res.redirect("/")
-                        } else if (!data) {
-                            req.flash("error", "Error to hash, choose another password")
-                            return res.redirect("/")
-                        }
-                        console.log(req.body.new_pass)
-                        User.findOneAndUpdate({ _id: req.user._id, username: req.user.username }, { password: data }, { new: true, timestamps: false }, (err, user) => {
-                            if (err) {
-                                req.flash("error", `${err.name}::${err.message}`)
-                                return res.redirect("/")
-                            } else if (!user) {
-                                req.flash("error", "error on update in DB")
-                                return res.redirect("/")
-                            }
-                            req.flash("success", "Successufuly account updated")
-                            return res.redirect("/")
+            })
+    } else if (old_pass && new_pass) {
+        if (typeof (old_pass) === "string" && old_pass.length > 6) {
+            if (typeof (new_pass) === "string" && new_pass === confirm_new_pass) {
+                if (old_pass.length > 6) {
+                    bcrypt.compare(old_pass, req.user.password)
+                        .then(value => {
+                            if (value) {
+                                return bcrypt.hash(new_pass, 12)
+                            } else { }
                         })
-                    })
-                })
-            } else {
-                req.flash("error", "new passwords will be equal")
-                return res.redirect("/")
+                        .then(hash => User.findByIdAndUpdate({ _id: req.body._id, username: req.user.username }, { password: hash }, { new: true }))
+                        .then(updatedUser => {
+                            req.flash("success", "User data successufly updated")
+                            return res.redirect("/")
+                        }).catch(e => {
+                            req.flash("error", `${e.name}::${e.message}`)
+                        })
+                } else { }
             }
         } else {
-            req.flash("error", "Password length will be greater than 6")
-            return res.redirect("/")
+
         }
-    } else {
-        req.flash("error", "Enter correct data")
-        return res.redirect("/")
-    }
+    } else { }
+    // if (req.body.username && req.body.username.length > 4) {
+    //     User.findOneAndUpdate({ _id: req.user._id, username: req.user.username }, { username: req.body.username }, { new: true }, (err, user) => {
+    //         if (err) {
+    //             req.flash("error", `${err.name}::${err.message}`)
+    //             return res.redirect("/")
+    //         } else if (!user) {
+    //             req.flash("error", "Dont update username")
+    //             return res.redirect("/")
+    //         }
+    //         req.flash("success", "User name successfuly updated")
+    //         return res.redirect("/")
+    //     })
+    // } else if (req.body.old_pass && req.body.new_pass && req.body.confirm_new_pass) {
+    //     if (req.body.old_pass.length > 5) {
+    //         if (req.body.new_pass === req.body.confirm_new_pass) {
+    //             bcrypt.compare(req.body.old_pass, req.user.password, (err, data) => {
+    //                 if (err) {
+    //                     req.flash("error", "error to compare passwords/ try later")
+    //                     return res.redirect("/")
+    //                 } else if (!data) {
+    //                     console.log(data)
+    //                     req.flash("error", "incorrect old password")
+    //                     return res.redirect("/")
+    //                 }
+    //                 hash = bcrypt.hash(req.body.new_pass, 12, (err, data) => {
+    //                     if (err) {
+    //                         req.flash("error", "Error to hashing new password")
+    //                         return res.redirect("/")
+    //                     } else if (!data) {
+    //                         req.flash("error", "Error to hash, choose another password")
+    //                         return res.redirect("/")
+    //                     }
+    //                     console.log(req.body.new_pass)
+    //                     User.findOneAndUpdate({ _id: req.user._id, username: req.user.username }, { password: data }, { new: true, timestamps: false }, (err, user) => {
+    //                         if (err) {
+    //                             req.flash("error", `${err.name}::${err.message}`)
+    //                             return res.redirect("/")
+    //                         } else if (!user) {
+    //                             req.flash("error", "error on update in DB")
+    //                             return res.redirect("/")
+    //                         }
+    //                         req.flash("success", "Successufuly account updated")
+    //                         return res.redirect("/")
+    //                     })
+    //                 })
+    //             })
+    //         } else {
+    //             req.flash("error", "new passwords will be equal")
+    //             return res.redirect("/")
+    //         }
+    //     } else {
+    //         req.flash("error", "Password length will be greater than 6")
+    //         return res.redirect("/")
+    //     }
+    // } else {
+    //     req.flash("error", "Enter correct data")
+    //     return res.redirect("/")
+    // }
 })
 
 router.post("/signup", ensureFromAuthenticated, (req, res, next) => {
-    if (typeof (req.body.username) === "string"
-        && req.body.username.length > 4
-        && req.body.username.length < 21) {
-        if ((typeof (req.body.password) === "string")
-            && (typeof (req.body.con_password) === "string")
-            && (req.body.con_password === req.body.password)
-            && (req.body.password.length > 5)
-        ) {
-            User.findOne({ username: req.body.username }, (err, user) => {
-                if (err) {
-                    next(err)
-                } else if (user) {
-                    req.flash("error", `username: ${req.body.username} not valid. Try another u-name!`)
-                    res.redirect("/auth")
-                } else {
-                    bcrypt.hash(req.body.password, 12)
-                        .then(hash => {
-                            User.create({
-                                username: req.body.username,
-                                password: hash
-                            }, (err, doc) => {
-                                if (err) {
-                                    req.flash("error", err)
-                                    res.redirect("/")
-                                } else {
-                                    next(null, doc)
-                                }
-                            })
-                        }).catch(e => {
-                            req.flash("error", e.toString())
-                            res.redirect("/")
-                        })
-                }
-            })
 
-        } else {
-            req.flash("error", "too short password or password dont equal password confirm")
-            res.redirect("/auth")
-        }
-    } else {
-        req.flash("error", "too short length username( > 4 || < 20)")
-        res.redirect("/auth")
-    }
+    // User.findOne({ username: req.body.username }, (err, user) => {
+    //     if (err) {
+    //         next(err)
+    //     } else if (user) {
+    //         req.flash("error", `username: ${req.body.username} not valid. Try another u-name!`)
+    //         res.redirect("/auth")
+    //     } else {
+    //         bcrypt.hash(req.body.password, 12)
+    //             .then(hash => {
+    //                 User.create({
+    //                     username: req.body.username,
+    //                     password: hash
+    //                 }, (err, doc) => {
+    //                     if (err) {
+    //                         req.flash("error", err)
+    //                         res.redirect("/")
+    //                     } else {
+    //                         next(null, doc)
+    //                     }
+    //                 })
+    //             }).catch(e => {
+    //                 req.flash("error", e.toString())
+    //                 res.redirect("/")
+    //             })
+    //     }
+    // })
+    const { username, password } = req.body
+    User.findOne({ username })
+        .then(user => {
+            if (user) {
+                req.flash("error", `username: ${req.body.username} not valid. Try another u-name!`)
+                return res.redirect("/auth")
+            } else if (!user) {
+                return bcrypt.hash(password, 12)
+            }
+        })
+        .then(hash => User.create({ username, password: hash }))
+        .then(user => next(null, user))
+        .catch(error => {
+            req.flash("error", `${error.name}::${error.message}`)
+            return res.redirect("/auth")
+        })
 }, passport.authenticate("local", { failureRedirect: "/auth" }), (req, res) => {
     req.flash("success", "successufly registred")
     res.redirect("/")
