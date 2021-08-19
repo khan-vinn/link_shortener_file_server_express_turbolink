b2a1405a4895c49f82d3e9e6423d6174b0a75627
Shakhan <khan.vinn@gmail.com>
2021-08-19 08:41:47 +0000

    replace to another package

---
+++ middleware/middleware.js
@@ -0,0 +1,29 @@
+function ensureNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    req.flash("error", "Please login or sign up to coninue")
+    res.redirect("/auth")
+}
+function ensureFromAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        req.flash("error", "You will login to app/ please logout to continue")
+        return res.redirect("/")
+    }
+    next()
+}
+
+function flashMessageProvideToRender(req, res, next) {
+    const succ = req.flash("success")
+    const err = req.flash("error")
+    if (succ.length > 0 && err.length > 0) {
+        res.locals.flash = ({ "error": err, "success": succ })
+    } else if (succ && succ.length > 0) {
+        res.locals.flash = ({ "success": succ })
+    } else if (err && err.length > 0) {
+        res.locals.flash = ({ "error": err })
+    }
+    next()
+}
+
+module.exports = { ensureNotAuthenticated, ensureFromAuthenticated, flashMessageProvideToRender }
