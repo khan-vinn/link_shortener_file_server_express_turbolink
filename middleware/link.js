3dd73ec24023502ef26fafb339a383cbc9c50bb5
Shakhan <khan.vinn@gmail.com>
2021-08-22 10:15:56 +0000

    link exist checeker bi id in parms

--- middleware/link.js
+++ middleware/link.js
@@ -1,3 +1,5 @@
+const { Link } = require("../models")
+
 function linkIdParamsValidate(req, res, next) {
     const { id } = req.params
     if (id && typeof (id) === "string" && id.length > 5) {
@@ -7,4 +9,27 @@ function linkIdParamsValidate(req, res, next) {
     res.redirect("/404")
 }
 
-module.exports = { linkIdParamsValidate }
+async function linkExistChecker(req, res, next) {
+    try {
+        const shortLink = req.params.id
+        if (shortLink && shortLink.length > 0) {
+            const link = await Link.findOne({ short_link: shortLink })
+            if (link) {
+                res.locals.link = link
+                return next()
+            } else {
+                req.flash("error", "Link Not Found")
+                return res.redirect("/")
+            }
+        } else {
+            res.locals.file = {}
+            return next()
+        }
+        next()
+    } catch {
+        req.flash("error", "Check your params")
+        return res.redirect("/")
+    }
+}
+
+module.exports = { linkIdParamsValidate,linkExistChecker }
